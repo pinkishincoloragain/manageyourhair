9bf57c8a67e57f5cf653846cea357e9e5ba85add
pinkishincoloragain <smb1103@gmail.com>
2021-11-27 18:36:22 +0000

    added buttons on detail page

--- App/Client/src/Components/Detail.js
+++ App/Client/src/Components/Detail.js
@@ -1,4 +1,9 @@
 import { useEffect, useState } from "react";
+import { Link } from 'react-router-dom';
+import Reservation from "assets/Reservation.jpeg"
+import Details from "assets/Details.jpeg"
+import Review from "assets/Review.png"
+
 import "styles/Detail.scss"
 
 export default function Detail(props) {
@@ -18,18 +23,46 @@ export default function Detail(props) {
         props.callback(false);
     }
 
+    const handleReserve = () => {
+    }
+
+    const handleDetail = () => {
+    }
+
     return (
         <div>
             <div className="fullPage" onClick={handleClose} />
-            <div className="detail">
-                <div>
+            <div>
+                <div className="nameLabel">
+                    {props.card.name}
+                </div>
+                <div className="detail">
                     <img src={props.card.image} alt={props.card.name} className="Image" />
-                    <div style={{ flex: 1 }}>
+                    <div>
                         <div className="detailHeader">
-                            {props.card.name}
                         </div>
                         <div className="daysWrapper">
                             {props.hours}
+                        </div>
+                        <div className="CardButtons" onClick={() => handleReserve()}>
+
+
+                        </div>
+                    </div>
+                </div>
+                <div className="buttons">
+                    <div className="btnHolder">
+                        <Link to={`./reservation/${props.card.shop_id}/${props.card.name.toString()}`} className="Reserve" style={{ textDecoration: "none", color: "black" }}>
+                            <div className="Reserve">
+                                <img src={Reservation} style={{ width: "4vw" }} className="icnBtn" />
+                                "Reservation"
+                            </div>
+                        </Link>
+                    </div>
+                    <div className="btnHolder">
+                        <div className="Review">
+                            <img src={Review} style={{ width: "4vw" }} className="icnBtn" />
+                            "Review"
                         </div>
                     </div>
                 </div>
